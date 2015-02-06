f74dcb39eefbfa1f39cbf5c7aaf7730edf8b0acd
Trey Griffith <trey.griffith@gmail.com>
2015-02-05 19:53:52 -0800

    only call listeners after the active records have been committed

--- lib/active_record_events.rb
+++ lib/active_record_events.rb
@@ -6,21 +6,9 @@ module ActiveRecordEvents
   extend ActiveSupport::Concern
   include EventEmitter
 
-  def after_create
-    self.emit(:create)
-  end
-
-  def after_update
-    self.emit(:update)
-  end
-
-  def after_destroy
-    self.emit(:destroy)
-  end
-
   included do
-    after_create :after_create
-    after_update :after_update
-    after_destroy :after_destroy
+    after_commit -> { emit(:create) }, on: :create
+    after_commit -> { emit(:update) }, on: :update
+    after_commit -> { emit(:destroy) }, on: :destroy
   end
 end
